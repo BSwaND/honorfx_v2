1473b2142745f525450d19c7f731099649cda9f5
BSwan <d4717549@gmail.com>
2021-02-11 18:34:44 +0200

    11.02.2021  18:00

--- .optimizedHTML/app/js/_custom.js
+++ .optimizedHTML/app/js/_custom.js
@@ -23,4 +23,93 @@ document.addEventListener("DOMContentLoaded", function() {
 	new WOW().init();
 
 
+	function SelectBlockInitialization(element){
+		let selectBlockInitializationClass = document.querySelectorAll('.'+element.selectBlockInitializationClass);
+		let selectBlockBtnClass = element.selectBlockBtnClass;
+		let selectBlockBtnClassActive = element.selectBlockBtnClassActive;
+		let popApBlockClass = element.popApBlockClass;
+		let popApBlockClassActive = element.popApBlockClassActive;
+		let tagLiClassInPopAp = element.tagLiClassInPopAp;
+
+		selectBlockInitializationClass.forEach((el) =>{
+			let selectTag =  el.querySelector('select');
+			templateBlockPopap(el.querySelector('.' + selectBlockBtnClass), selectTag.options);
+			deployValueInSelectOnClick(el);
+
+			el.querySelectorAll('.'+selectBlockBtnClass).forEach((el) =>{
+				el.addEventListener('click', function (e){
+					if (e.target  === this || e.target == this.querySelector('span')) {
+						console.log('click btn')
+						console.log(e.target)
+						let curentPopApBlokUL =  this.querySelector(`.${popApBlockClass}`);
+						this.classList.toggle(selectBlockBtnClassActive);
+						curentPopApBlokUL.classList.toggle(popApBlockClassActive);
+
+						// document.addEventListener('click', function (){
+						// 		curentPopApBlokUL.classList.remove(popApBlockClassActive);
+						// 	},{capture:true}
+						// )
+
+					} else {
+						// console.log('no btn')
+						console.log(e.target)
+					}
+
+
+				})
+			})
+		})
+
+		function templateBlockPopap(parentBlock, elementItaration){
+			let outerDiv = document.createElement('div');
+			let ul = document.createElement('ul');
+			outerDiv.className = popApBlockClass;
+			for(let i = 0; elementItaration.length > i; i++){
+				if(elementItaration[i].value){
+					ul.innerHTML += `<li data-value="${elementItaration[i].value}" class="${tagLiClassInPopAp}">${elementItaration[i].innerHTML}</li>`;
+				}
+			}
+			if(parentBlock.parentNode.querySelector('input')){
+				let input = parentBlock.parentNode.querySelector('input');
+				outerDiv.append(input);
+				input.addEventListener('click', function (e){
+					console.log(e)
+				},{capture:true})
+			}
+			outerDiv.append(ul);
+			parentBlock.append(outerDiv);
+		}
+
+		function deployValueInSelectOnClick(el){
+			let liOptions = el.querySelectorAll('.'+tagLiClassInPopAp);
+
+			liOptions.forEach((i)=>{
+				i.addEventListener('click', function (){
+					el.querySelector('select').value = this.getAttribute('data-value');
+					el.querySelector('.'+selectBlockBtnClass + ' span').innerHTML = this.innerHTML;
+					removeClass(el)
+					console.log('select>value = ' + el.querySelector('select').value)
+				})
+			})
+		}
+
+		function removeClass(el){
+			el.querySelector(`.${popApBlockClass}`)
+				.classList.remove(popApBlockClassActive);
+			el.querySelector(`.${selectBlockBtnClass}`)
+				.classList.remove(selectBlockBtnClassActive);
+		}
+	}
+
+
+	let selectBlockInitializationColRow = new SelectBlockInitialization({
+		selectBlockInitializationClass: 'qqq',
+		selectBlockBtnClass: 'select-block_btn',
+		selectBlockBtnClassActive: 'select-block_btn__open',
+		popApBlockClass: 'select-block_popap',
+		popApBlockClassActive: 'select-block_popap__active',
+		tagLiClassInPopAp: 'select-block_popap__li', //default
+	})
+
+
 });
--- templates/custom/html/geo_location.php
+++ templates/custom/html/geo_location.php
@@ -3,16 +3,21 @@
 	 * Start
 	 * Geo location for Thailand
 	 */
-	$locationThailand = false;
-	$getIrl =  'http://api.sypexgeo.net/json/'.$_SERVER['REMOTE_ADDR'];
-	$geo =  json_decode(file_get_contents($getIrl));
-	$countryLocationName = $geo->country->name_en;
 	
-	switch ($countryLocationName){
-		case 'Thailand' ;
-			$locationThailand = true;
-			break ;
-		default:
-			$locationThailand = false;
+	if(!$document->countryLocationName){
+		
+		$document->locationThailand = false;
+		$getIrl =  'http://api.sypexgeo.net/json/'.$_SERVER['REMOTE_ADDR'];
+		$geo =  json_decode(file_get_contents($getIrl));
+		$document->countryLocationName = $geo->country->name_en;
+		                                                        
+		switch ($document->countryLocationName){
+			case 'Thailand' ;
+				$document->locationThailand = true;
+				break ;
+			default:
+				$document->locationThailand = false;
+		}
+		   
 	}
 	/** End  */
